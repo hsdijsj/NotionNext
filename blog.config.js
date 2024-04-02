61a10311cd9bedd6290074d47752eb18e6ee20e3
hsdijsj <87793903+hsdijsj@users.noreply.github.com>
2024-04-02 16:27:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 Open Cirrus
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Open Cirrus', // 您的昵称 例如 Open Cirrus
   BIO: process.env.NEXT_PUBLIC_BIO || '一个曾在美院学宗教的艺术自媒体创作者🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://caiyaxin2019.asia', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
